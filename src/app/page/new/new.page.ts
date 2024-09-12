bfa3e19eadff7dd732c6b76ae41ed84c6ef5d07e
Momaco12 <momaco.lim@gmail.com>
2024-09-12 01:00:00 -0700

    segunda

--- src/app/page/new/new.page.ts
+++ src/app/page/new/new.page.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 
@@ -11,6 +12,7 @@ export class NewPage implements OnInit {
 
 
   mio: string[]= [];
+  otro: string[]= [];
 
 
   myInput: string = '';
@@ -20,13 +22,24 @@ export class NewPage implements OnInit {
   enviar(){
     this.mio.push(this.myInput)
     console.log(this.myInput)
-    this.myInput = ''
+    const body = { question_text: this.myInput };
+    
+    this.http.post('http://127.0.0.1:8000/path/v1/echo/', body).subscribe({
+      next: (res: any) => {
+        this.otro.push(res.response)  // Asignar la respuesta recibida
+        this.myInput = ''
+        console.log(this.otro)
+      },
+      error: (error) => {
+        console.error('Error al enviar la pregunta', error);
+      }}
+    );
     
     
   }
 
   
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
   }
